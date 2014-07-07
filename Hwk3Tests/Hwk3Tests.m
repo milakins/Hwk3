e6c4d70a14102beba0d1ce7a3c2955a82f5d7ecb
Mila <jmilam@jmilam-macbookair.roam.corp.google.com>
2014-07-06 18:10:40 -0700

    Initial Commit

---
+++ Hwk3Tests/Hwk3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Hwk3Tests.m
+//  Hwk3Tests
+//
+//  Created by jmilam on 7/6/14.
+//  Copyright (c) 2014 Jen Milam. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hwk3Tests : XCTestCase
+
+@end
+
+@implementation Hwk3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
